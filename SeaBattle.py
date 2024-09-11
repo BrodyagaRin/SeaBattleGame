50d8b46e6b3a3269ba519154ce4b975b87bf1e38
Name <email@example.ru>
2024-09-11 21:54:34 +0300

    Update SeaBattle.py

--- SeaBattle.py
+++ SeaBattle.py
@@ -1,52 +1,5 @@
+from random import randint
 
-    def game_cycle(self):
-        num = 0
-        while True:
-            print("-" * 20)
-            print("Player`s board:")
-            print(self.us.board)
-            print("-" * 20)
-            print("System`s board:")
-            print(self.ai.board)
-            if num % 2 == 0:
-                print("-" * 20)
-                print(f"It`t {player_name} turn!")
-                repeat = self.us.move()
-            else:
-                print("-" * 20)
-                print("It`s system`s turn!")
-                repeat = self.ai.move()
-            if repeat:
-                num -= 1
-
-            if self.ai.board.count == 7:
-                print("-" * 20)
-                print("Player won!")
-                break
-
-            if self.us.board.count == 7:
-                print("-" * 20)
-                print("System won!")
-                break
-            num += 1
-
-    def start(self):
-        self.greet()
-        self.loop()
-
-def ask_Player(player_name):
-    while True:
-        x, y = map(int, input(f" Your decision {player_name}: ").split())
-
-        if (x < 0 or x > 2) or (y < 0 or y > 2):
-            print(" Write coordinates between 0 and 2 ")
-            continue
-
-        if level[x][y] != " ":
-           print(" This spot is taken ")
-           continue
-        break
-    return x, y
 class Dot:
     def __init__(self, x, y):
         self.x = x
@@ -57,63 +10,21 @@ class Dot:
 
     def __repr__(self):
         return f"({self.x}, {self.y})"
-interface_intro()
 
-level = [[" "] * 6 for i in range(6)]
-
-
-def level_structure_top():
-    print("That`s your board:")
-    print()
-    print(f" y/x| 1 | 2 | 3 | 4 | 5 | 6 |")
-    print("  --------------- ")
-
-
-def level_structure_body():
-    for i, row in enumerate(level):
-        row = f"  {i} | {' | '.join(row)} | "
-        print(row)
-        print("  --------------- ")
-    print()
-
-
-def ask_Player(player_name):
-    while True:
-        x, y = map(int, input(f" Your decision {player_name}: ").split())
-
-        if (x < 0 or x > 2) or (y < 0 or y > 2):
-            print(" Write coordinates between 0 and 2 ")
-            continue
-
-        if level[x][y] != " ":
-           print(" This spot is taken ")
-           continue
-        break
-    return x, y
-
-
-class Dot:
-
- def __init__(self, x, y):
-    self.x = x
-    self.y = y
-
-def __eq__(self, other):
-    return self.x == other.x and self.y == other.y
-
-def __repr__(self):
-    return f"({self.x}, {self.y})"
 
 class BoardException(Exception):
     pass
 
-class BoardOutsideException(BoardException):
-    def __str__(self):
-        return "Your shot is outside the board! Try again."
 
-class BoardSpotUsedException(BoardException):
+class BoardOutException(BoardException):
     def __str__(self):
-        return "This spot was already shot!"
+        return "You are trying to shoot out of the board!"
+
+
+class BoardUsedException(BoardException):
+    def __str__(self):
+        return "You have already shot this cell"
+
 
 class BoardWrongShipException(BoardException):
     pass
@@ -135,7 +46,6 @@ class Ship:
 
             if self.o == 0:
                 cur_x += i
-
             elif self.o == 1:
                 cur_y += i
 
@@ -151,43 +61,22 @@ class Board:
     def __init__(self, hid=False, size=6):
         self.size = size
         self.hid = hid
-
         self.count = 0
-
         self.field = [["O"] * size for _ in range(size)]
-
         self.busy = []
         self.ships = []
 
     def add_ship(self, ship):
-
         for d in ship.dots:
             if self.out(d) or d in self.busy:
                 raise BoardWrongShipException()
         for d in ship.dots:
             self.field[d.x][d.y] = "■"
             self.busy.append(d)
-
         self.ships.append(ship)
-        self.contour(ship)
-
-    def contour(self, ship, verb=False):
-        near = [
-            (-1, -1), (-1, 0), (-1, 1),
-            (0, -1), (0, 0), (0, 1),
-            (1, -1), (1, 0), (1, 1)
-        ]
-        for d in ship.dots:
-            for dx, dy in near:
-                cur = Dot(d.x + dx, d.y + dy)
-                if not (self.out(cur)) and cur not in self.busy:
-                    if verb:
-                        self.field[cur.x][cur.y] = "."
-                    self.busy.append(cur)
 
     def __str__(self):
-        res = ""
-        res += "  | 1 | 2 | 3 | 4 | 5 | 6 |"
+        res = "x/y| 1 | 2 | 3 | 4 | 5 | 6 |"
         for i, row in enumerate(self.field):
             res += f"\n{i + 1} | " + " | ".join(row) + " |"
 
@@ -196,7 +85,7 @@ class Board:
         return res
 
     def out(self, d):
-        return not ((0 <= d.x < self.size) and (0 <= d.y < self.size))
+        return not (0 <= d.x < self.size and 0 <= d.y < self.size)
 
     def shot(self, d):
         if self.out(d):
@@ -213,25 +102,38 @@ class Board:
                 self.field[d.x][d.y] = "X"
                 if ship.lives == 0:
                     self.count += 1
-                    self.contour(ship, verb=True)
-                    print("Ship was destroyed! Congratulations!")
-                    return False
+                    self.contour(ship)
+                    print("Ship destroyed!")
+                    return True  # Возвращаем True, если попадание
                 else:
-                    print("Ship was damaged! You`re almost there!")
-                    return True
+                    print("Ship was hit!")
+                    return True  # Возвращаем True, если попадание
 
-        self.field[d.x][d.y] = "."
-        print("You missed!")
-        return False
+        self.field[d.x][d.y] = "T"  # Используем букву T для промахов
+        print("Miss!")
+        return False  # Возвращаем False, если промах
+
+    def contour(self, ship):
+        near = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1), (0, 0), (0, 1),
+            (1, -1), (1, 0), (1, 1)
+        ]
+        for d in ship.dots:
+            for dx, dy in near:
+                cur = Dot(d.x + dx, d.y + dy)
+                if not self.out(cur) and cur not in self.busy and self.field[cur.x][cur.y] == "O":
+                    self.field[cur.x][cur.y] = "T"
 
     def begin(self):
         self.busy = []
 
 
 class Player:
-    def __init__(self, board, enemy):
+    def __init__(self, board, enemy, name):
         self.board = board
         self.enemy = enemy
+        self.name = name
 
     def ask(self):
         raise NotImplementedError()
@@ -246,42 +148,74 @@ class Player:
                 print(e)
 
 
-class AI(Player):
-    def ask(self):
-        d = Dot(randint(0, 5), randint(0, 5))
-        print(f"System`s turn: {d.x + 1} {d.y + 1}")
-        return d
-
-
 class User(Player):
     def ask(self):
         while True:
-            cords = input(f" It`s {player_name} turn: ").split()
+            cords = input(f"{self.name}, your move (format: x y): ").split()
 
             if len(cords) != 2:
-                print(" Write two coordinates for x and y! ")
+                print("Enter 2 coordinates!")
                 continue
 
             x, y = cords
 
             if not (x.isdigit()) or not (y.isdigit()):
-                print(" You can only write digits. Try again! ")
+                print("Enter numbers!")
                 continue
 
             x, y = int(x), int(y)
 
+            if x < 1 or x > self.board.size or y < 1 or y > self.board.size:
+                print("Coordinates are out of range!")
+                continue
+
             return Dot(x - 1, y - 1)
+
+
+class AI_System(Player):
+    def ask(self):
+        x = randint(0, 5)
+        y = randint(0, 5)
+        print(f"Computer-pirate's move: {x + 1} {y + 1}")
+        return Dot(x, y)
 
 
 class Game:
     def __init__(self, size=6):
         self.size = size
-        pl = self.random_board()
-        co = self.random_board()
-        co.hid = True
+        self.player_name = ""
+        self.ai = None
+        self.us = None
 
-        self.ai = AI(co, pl)
-        self.us = User(pl, co)
+    def greet(self):
+        print(f"Welcome to the Sea Battle Game, commander!")
+        print("-------------------")
+        print("Write coordinates to place your symbol in x y format!")
+        print("-------------------")
+        print("Press Enter to start your game!")
+        print("-------------------")
+        input()
+        self.player_name = input("Enter your name, commander: ")
+
+    def show_board_options(self):
+        options = []
+        for _ in range(3):  # Number of board options
+            board = self.random_board()
+            options.append(board)
+
+        option_index = 0
+        while True:
+            print(f"Choose your starting board, {self.player_name}! Board option {option_index + 1}:")
+            print(options[option_index])
+            print("-------------------")
+            print("Type 'Next' to see the next option or 'Ok' to select this board.")
+            choice = input("Your choice: ").strip().lower()
+            if choice == 'next':
+                option_index = (option_index + 1) % len(options)
+            elif choice == 'ok':
+                return options[option_index]
+            else:
+                print("Invalid input! Please type 'Next' or 'Ok'.")
 
     def random_board(self):
         board = None
@@ -298,7 +232,7 @@ class Game:
                 attempts += 1
                 if attempts > 2000:
                     return None
-                ship = Ship(Dot(randint(0, self.size), randint(0, self.size)), l, randint(0, 1))
+                ship = Ship(Dot(randint(0, self.size - 1), randint(0, self.size - 1)), l, randint(0, 1))
                 try:
                     board.add_ship(ship)
                     break
@@ -307,53 +241,48 @@ class Game:
         board.begin()
         return board
 
-    def interface_intro():
-        print("Welcome to the Sea Battle game!")
-        print("-------------------")
-        print("Write coordinates to place your symbol in x y format!")
-        print("-------------------")
-        print("Press Enter to start your game!")
-        input()
-        print("-------------------")
-        player_name = str(input("Write Player`s name:"))
-        print("Press Enter to continue")
-        input()
-
     def loop(self):
         num = 0
         while True:
             print("-" * 20)
-            print(f"{player_name}'s battlefield board:")
+            print(f"{self.player_name}'s board:")
             print(self.us.board)
             print("-" * 20)
-            print("System`s battlefield board:")
+            print("Computer-pirate's board:")
             print(self.ai.board)
             if num % 2 == 0:
                 print("-" * 20)
-                print(f" It`s {player_name} turn!")
+                print(f"{self.player_name}'s turn!")
                 repeat = self.us.move()
+                if not repeat:
+                    num += 1
             else:
                 print("-" * 20)
-                print("It`s system`s turn!")
+                print("Computer-pirate's turn!")
                 repeat = self.ai.move()
-            if repeat:
-                num -= 1
+                if not repeat:
+                    num += 1
 
             if self.ai.board.count == 7:
                 print("-" * 20)
-                print(f" {player_name} won!")
+                print(f"{self.player_name} won!")
                 break
 
             if self.us.board.count == 7:
                 print("-" * 20)
-                print("System won! Get your revanche!")
+                print("Computer-pirate won! Try again, commander")
                 break
-            num += 1
 
     def start(self):
         self.greet()
+        ai_board = self.random_board()
+        user_board = self.show_board_options()
+        self.us = User(user_board, ai_board, self.player_name)
+        self.ai = AI_System(ai_board, self.us.board, "Computer-pirate")
+        self.ai.board.hid = True
         self.loop()
 
 
-g = Game()
-g.start()
+if __name__ == "__main__":
+    g = Game()
+    g.start()
